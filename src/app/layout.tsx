c31e676488a65a5618c023d901675ae59f61abca
Carlos Romero <cmromero.dev@gmail.com>
2025-02-10 21:57:15 -0600

    feat: Add Vercel Speed Insights to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import Template from "./_components/template"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -56,6 +57,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Template>{children}</Template>
+        <SpeedInsights />
       </body>
     </html>
   )
