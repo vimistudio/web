7c3f1a054935d3db1c2e16490fdf198016912aa2
Carlos Romero <cmromero.dev@gmail.com>
2025-02-12 01:53:31 -0600

    chore: Refactor project directories and files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Template from "./_components/template";
+import Template from "../components/template";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
 
