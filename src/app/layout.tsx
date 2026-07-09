82b9e399075a9b2894f0194090a441a62880b061
Carlos Romero <cmromero.dev@gmail.com>
2026-07-08 23:03:01 -0600

    feat: iOS standalone polish

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -105,7 +105,7 @@ export const metadata: Metadata = {
       { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
       { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
     ],
-    apple: [{ url: "/apple-touch-icon.png" }],
+    apple: [{ url: "/apple-icon", sizes: "180x180", type: "image/png" }],
     other: [
       {
         rel: "mask-icon",
